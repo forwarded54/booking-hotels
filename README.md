694287160674666fb5bc1fc850a21563cba809f6
Daniil Ivanov <101810094+forwarded54@users.noreply.github.com>
2022-03-21 15:54:24 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+<img alt="imgur photo" src="https://i.imgur.com/l6XE96P.png">
---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--prev",
+      prevEl: ".hotel-slider__button--next",
+    },
+    effect: "coverflow",
+  });
+
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+
+  var menuButton = $('.menu-button');
+  menuButton.on('click', function () {
+    console.log('Клик по кнопке меню');
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $('[data-toggle=modal]')
+  modalButton.on('click', openModal);
+  var closeModalButton = $('.modal__close')
+  closeModalButton.on('click', closeModal);
+
+  function openModal () {
+    var targetModal = $(this).attr('data-href')
+    $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
+    $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
+  }
+  function closeModal (event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+
+  // Обработка форм
+  $('.form').each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Укажите имя",
+          minlength: "Имя должно быть не короче двух букв"
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Телефон обязателен",
+        },
+      },
+    });
+  });
+
+  AOS.init();
+
+});
